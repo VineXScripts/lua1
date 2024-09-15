72cd6a655279ef350ed9491cd80072649fb47691
VineXScripts <mackinnonnixon290@gmail.com>
2024-09-15 13:08:13 -0300

    Update api.lua

--- api.lua
+++ api.lua
@@ -1,1 +1,10 @@
-# lua1
+local Notif = loadstring(game:HttpGet("https://raw.githubusercontent.com/SkidBinOnTop/NotiLib/main/source.lua"))()
+
+Notif:CreateNotification({Title = 'Emulsia', Content = 'Brought By Vine And Dex', Length = 5, Buttons = {
+	[1] = {
+		Title = 'Dismiss', 
+		ClosesUI = true, 
+		Callback = function()
+    end
+	}
+}})
